aac10f990b43a61df5911edbca9f43d29c165225
Weijun Huang <69240466+shijua@users.noreply.github.com>
2026-02-10 13:09:14 +0000

    Initial Commit

---
+++ The-Infinite-Terrarium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The-Infinite-Terrarium
+//
+//  Created by 黄伟俊 on 10/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The-Infinite-Terrarium/The_Infinite_TerrariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Infinite_TerrariumApp.swift
+//  The-Infinite-Terrarium
+//
+//  Created by 黄伟俊 on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct The_Infinite_TerrariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
